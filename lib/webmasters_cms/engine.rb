bc79e601e54b9fe9c568d7e2d4b9702cd160885a
cgallitz <cgallitzendoe@gmail.com>
2014-05-22 10:23:23 +0200

    configure to use rspec

--- lib/webmasters_cms/engine.rb
+++ lib/webmasters_cms/engine.rb
@@ -1,5 +1,13 @@
 module WebmastersCms
   class Engine < ::Rails::Engine
     isolate_namespace WebmastersCms
+    
+    config.generators do |g|
+      g.orm :active_record
+      g.template_engine :erb
+      g.test_framework :rspec #, :fixture => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+      g.view_specs false
+    end
   end
 end
