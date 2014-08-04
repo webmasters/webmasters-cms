bf33c63e2fe4a79af347ae1d8295868a418d2347
cgallitz <cgallitzendoe@gmail.com>
2014-08-04 09:53:16 +0200

    refs #685 @0.15  * bug fix

---
+++ app/assets/javascripts/webmasters_cms/admin/previewSearchResult.js
@@ -0,0 +1,32 @@
+jQuery(function($) {
+
+  $('.searchResult').on("click", function() {
+    event.preventDefault();
+  });
+
+  $('#translation_title').on("keyup", function() {
+    var element = $(this);
+    var strLength = 55;
+    if (element.val().length <= strLength) {
+      $('.big-link span').html(element.val());
+    } else {
+      $('.big-link span').html(element.val().replace(new RegExp("^(.{" + strLength + "}[^\s]*).*"), "$1") + '&nbsp;...');
+    }
+  });
+
+  $('#translation_meta_description').on("keyup", function() {
+    var element = $(this);
+    var strLength = 155;
+    if (element.val().length <= strLength) {
+      $('.metadesc').html(element.val());
+    } else {
+      $('.metadesc').html(element.val().replace(new RegExp("^(.{" + strLength + "}[^\s]*).*"), "$1") + '&nbsp;...');
+    }
+  });
+
+  $('#translation_local_path').on("keyup", function() {
+    var element = $(this);
+    $('.url').html(location.host + '/' + element.val());
+  });
+
+});
