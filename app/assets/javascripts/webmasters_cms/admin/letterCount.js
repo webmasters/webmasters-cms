99a2071f23ad7c0a1dc8432966e24e4b641f53ba
cgallitz <c.gallitzendoerfer@webmasters.de>
2014-08-05 15:26:19 +0200

    refs #685 @0.5  * refactoring to new generated form prefix

--- app/assets/javascripts/webmasters_cms/admin/letterCount.js
+++ app/assets/javascripts/webmasters_cms/admin/letterCount.js
@@ -1,29 +1,31 @@
 jQuery(function($) {
   if ($('#titleLength').length) {
-    $('#titleLength span').html($('#translation_title').val().length);
-    $('#metaDescLength span').html($('#translation_meta_description').val().length);
+    var form_prefix = '#page_translations_attributes_0_';
+
+    $('#titleLength span').html($(form_prefix + 'title').val().length);
+    $('#metaDescLength span').html($(form_prefix + 'meta_description').val().length);
     if ($('#titleLength').val().length < 56) {
       $('#titleLength').removeClass('warning').addClass('ok');
     } else {
       $('#titleLength').removeClass('ok').addClass('warning');
-    }  
+    }
         if ($('#metaDescLength').val().length < 156) {
       $('#metaDescLength').removeClass('warning').addClass('ok');
     } else {
       $('#metaDescLength').removeClass('ok').addClass('warning');
     }
 
-    $('#translation_title').on('keyup', function() {
+    $(form_prefix + 'title').on('keyup', function() {
       var element = $(this);
       $('#titleLength span').html(element.val().length);
       if (element.val().length < 56) {
         $('#titleLength').removeClass('warning').addClass('ok');
       } else {
         $('#titleLength').removeClass('ok').addClass('warning');
-      }  
+      }
     });
 
-      $('#translation_meta_description').on('keyup', function() {
+      $(form_prefix + 'meta_description').on('keyup', function() {
       var element = $(this);
       $('#metaDescLength span').html(element.val().length);
       if (element.val().length < 156) {
--- app/assets/javascripts/webmasters_cms/admin/previewSearchResult.js
+++ app/assets/javascripts/webmasters_cms/admin/previewSearchResult.js
@@ -1,35 +1,40 @@
 jQuery(function($) {
 
-  $('#translation_title').on("keyup", function() {
+  var form_prefix = '#page_translations_attributes_0_';
+
+  $(form_prefix + 'title').on("keyup", function() {
     var element = $(this);
+    var target = $('.big-link span');
     var strLength = 55;
     if (element.val().length === 0) {
-      $('.big-link span').html("Preview Link")
+      target.html("Preview Link")
     } else if (element.val().length <= strLength) {
-      $('.big-link span').html(element.val());
+      target.html(element.val());
     } else {
-      $('.big-link span').html(element.val().replace(new RegExp("^(.{" + strLength + "}[\s]*).*"), "$1") + '&nbsp;...');
+      target.html(element.val().replace(new RegExp("^(.{" + strLength + "}[\s]*).*"), "$1") + '&nbsp;...');
     }
   });
 
-  $('#translation_meta_description').on("keyup", function() {
+  $(form_prefix + 'meta_description').on("keyup", function() {
     var element = $(this);
+    var target = $('.metadesc');
     var strLength = 155;
     if (element.val().length === 0) {
-      $('.metadesc').html("This is the meta description for the page");
+      target.html("This is the meta description for the page");
     } else if (element.val().length <= strLength) {
-      $('.metadesc').html(element.val());
+      target.html(element.val());
     } else {
-      $('.metadesc').html(element.val().replace(new RegExp("^(.{" + strLength + "}[\s]*).*"), "$1") + '&nbsp;...');
+      target.html(element.val().replace(new RegExp("^(.{" + strLength + "}[\s]*).*"), "$1") + '&nbsp;...');
     }
   });
 
-  $('#translation_local_path').on("keyup", function() {
+  $(form_prefix + 'local_path').on("keyup", function() {
     var element = $(this);
+    var target = $('.url');
     if (element.val().length === 0) {
-      $('.url').html('www.previewurl.com')
+      target.html('www.previewurl.com')
     } else {
-      $('.url').html(location.host + '/' + element.val());
+      target.html(location.host + '/' + element.val());
     }
   });
 
