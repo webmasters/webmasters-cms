5b55c016c656ef65d3158b1daa451e50492afc2c
cgallitz <cgallitzendoe@gmail.com>
2014-08-06 11:36:26 +0200

    refs #689 @2.0  * added all translations to tree  * new show page selection  * minor translations

---
+++ app/assets/javascripts/webmasters_cms/admin/showPage.js
@@ -0,0 +1,11 @@
+jQuery(function($) {
+  $(function(){
+    $('#showPage').on('click', function () {
+      var url = 'http://' + location.host + '/' + $(this).val();
+      if (url) {
+        window.open(url);
+      }
+      return false;
+    });
+  });
+});
