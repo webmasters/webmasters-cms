b7035ef8d570c52ad0f53c7ce6bb1a3ef5ca3a43
cgallitz <cgallitzendoe@gmail.com>
2014-08-08 15:01:10 +0200

    refs #688 @0.5  * renamed file to get under size 100

--- db/migrate/20140731091359_rename_table_webmasters_cms_page_versions_webmasters_cms_page_translation_versions.rb
+++ db/migrate/20140731091359_rename_table_webmasters_cms_page_versions_webmasters_cms_page_translation_versions.rb
@@ -1,4 +1,4 @@
-class RenameTableWebmastersCmsPageVersionsToWebmastersCmsPageTranslationVersions < ActiveRecord::Migration
+class RenameTableWebmastersCmsPageVersionsWebmastersCmsPageTranslationVersions < ActiveRecord::Migration
   def change
     remove_index :webmasters_cms_page_versions, :page_id
     rename_table :webmasters_cms_page_versions, :webmasters_cms_page_translation_versions
