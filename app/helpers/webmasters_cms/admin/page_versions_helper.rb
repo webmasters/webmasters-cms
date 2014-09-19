3bf875f7e0dff51cf24fb831878ab0262388beca
cgallitz <cgallitzendoe@gmail.com>
2014-09-19 11:59:40 +0200

    refs #740 @0.5  * last created version is default in versions chooser

--- app/helpers/webmasters_cms/admin/page_versions_helper.rb
+++ app/helpers/webmasters_cms/admin/page_versions_helper.rb
@@ -1,10 +1,12 @@
 module WebmastersCms
   module Admin
     module PageVersionsHelper
-      def page_versions_for_select
+      def page_versions_in_array
+        array = []
         collection_without_current_version.collect do |r|
-          [ r.version.to_s, r.version ]
+           array << [ r.version.to_s, r.version ]
         end
+        array
       end
     end
   end
