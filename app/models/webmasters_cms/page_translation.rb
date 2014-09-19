daf64d000bd185cc45692caa34a86c4eac6892b4
cgallitz <cgallitzendoe@gmail.com>
2014-09-19 13:05:18 +0200

    refs #742 @1.0  * added new validation to model

--- app/models/webmasters_cms/page_translation.rb
+++ app/models/webmasters_cms/page_translation.rb
@@ -7,7 +7,8 @@ module WebmastersCms
       if_changed: [:name, :local_path, :title, :meta_description, :body, :language],
       non_versioned_columns: [:page_id]
 
-    validates :name, :local_path, uniqueness: {:scope => [:page_id, :language]}
+    validates :name, :local_path, uniqueness: {scope: [:page_id, :language]}
+    validates :name, :local_path, uniqueness: {scope: :language}
 
     validates :local_path, length: { maximum: 255 }
 
@@ -21,7 +22,7 @@ module WebmastersCms
 
     validates :local_path, format: { with: /\A[a-zA-Z0-9\-\_\/\.]+\z/, allow_blank: true }
 
-    validates :language, presence: true, active_languages: true, uniqueness: {scope: :page_id}
+    validates :language, presence: true, active_languages: true, uniqueness: {scope: :page_id }
 
     def current_version
       versions.where(version: version).first
