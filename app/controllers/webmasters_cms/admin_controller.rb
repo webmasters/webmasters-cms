7f058fc30d4aef7f5d2e1380e76ec5d8cba473e3
Christian Eichhorn <c.eichhorn@webmasters.de>
2014-10-16 11:14:02 +0200

    * helper fixed

--- app/controllers/webmasters_cms/admin_controller.rb
+++ app/controllers/webmasters_cms/admin_controller.rb
@@ -2,5 +2,6 @@ require_dependency "webmasters_cms/application_controller"
 
 module WebmastersCms
   class AdminController < ApplicationController
+    helper 'webmasters_cms/admin/form'
   end
 end
--- app/controllers/webmasters_cms/application_controller.rb
+++ app/controllers/webmasters_cms/application_controller.rb
@@ -1,4 +1,4 @@
 module WebmastersCms
-  class ApplicationController < ActionController::Base
+  class ApplicationController < ::ApplicationController
   end
 end
