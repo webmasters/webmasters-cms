ca9a39712802010452e77abecaac1dd997fb90b2
Christian Eichhorn <c.eichhorn@webmasters.de>
2018-07-04 08:05:35 +0200

    * database not exists fixed

--- app/models/webmasters_cms/application_record.rb
+++ app/models/webmasters_cms/application_record.rb
@@ -2,4 +2,10 @@ class WebmastersCms::ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   self.table_name = nil # fix for table_name detection
   self.belongs_to_required_by_default = false if self.respond_to?(:belongs_to_required_by_default) # for Rails 5.2
+
+  def self.table_exists?
+    super
+  rescue ActiveRecord::NoDatabaseError
+    false
+  end
 end
