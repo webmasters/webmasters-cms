0cf35f52909e59a4fe6a71e8d6db148f33bd4e62
Christian Eichhorn <c.eichhorn@webmasters.de>
2020-12-14 07:37:05 +0100

    * validator fixed

--- app/validators/webmasters_cms/active_languages_validator.rb
+++ app/validators/webmasters_cms/active_languages_validator.rb
@@ -1,7 +1,7 @@
 class WebmastersCms::ActiveLanguagesValidator < ActiveModel::EachValidator
 
   def validate_each(record, attr_name, value)
-    unless ActiveLanguage.active?(value)
+    unless WebmastersCms::ActiveLanguage.active?(value)
       record.errors.add(attr_name, :not_active, options)
     end
   end
